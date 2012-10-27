1e55a55f54fba40ba3c40dc05df96b60aee29c3b
David Collinsworth <ttctw@asu.edu>
2012-10-27 16:58:04 -0700

    GitHub for Mac: Throw-away commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '3afe9f24bef44401485c9f41e0a370f76d758d23704884776e7fac3c6d2723b2b0563116d41179ab596b85388d0e80175c78b001475071d7f2f9895cd170d479'
